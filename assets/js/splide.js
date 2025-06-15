b56c4b5744553449f63e3187b49bb91b390054bc
[Rohit Kumar] <[bytepedia.in@gmail.com]>
2025-06-15 11:34:48 +0530

    refactor: wrap Splide initialization in DOMContentLoaded event listener for better structure

--- assets/js/splide.js
+++ assets/js/splide.js
@@ -1,18 +1,22 @@
 // Featured Image Carousel
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide("#featured-image-carousel", {
+    type: "fade",
+    rewind: true,
+    autoplay: true,
+    interval: 5000,
+    arrows: true, // default for larger screens
+    breakpoints: {
+      768: { arrows: false }, // disable arrows below 768px
+    },
+  }).mount();
+});
 
-new Splide("#featured-image-carousel", {
-  type: "fade",
-  rewind: true,
-  autoplay: true,
-  interval: 5000,
-}).mount();
-
-
-document.addEventListener( 'DOMContentLoaded', function () {
-  new Splide( '#thumbnail-carousel', {
-		fixedWidth: 100,
-		gap       : 10,
-		rewind    : true,
-		pagination: false,
-  } ).mount();
-} );
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide("#thumbnail-carousel", {
+    fixedWidth: 100,
+    gap: 10,
+    rewind: true,
+    pagination: false,
+  }).mount();
+});
