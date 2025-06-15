d944f0525b982258ab4e63af36e4543031fbb043
[Rohit Kumar] <[bytepedia.in@gmail.com]>
2025-06-15 11:31:04 +0530

    commit

--- assets/js/splide.js
+++ assets/js/splide.js
@@ -1,7 +1,18 @@
 // Featured Image Carousel
+
 new Splide("#featured-image-carousel", {
   type: "fade",
   rewind: true,
   autoplay: true,
   interval: 5000,
 }).mount();
+
+
+document.addEventListener( 'DOMContentLoaded', function () {
+  new Splide( '#thumbnail-carousel', {
+		fixedWidth: 100,
+		gap       : 10,
+		rewind    : true,
+		pagination: false,
+  } ).mount();
+} );
